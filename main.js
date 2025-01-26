9a61163e6196c2947413ae5efea7d387f8332644
Ivan Adamo <ivan.adamo.8@gmail.com>
2025-01-26 19:23:35 -0300

    practica finalizada

---
+++ main.js
@@ -0,0 +1,26 @@
+const numero = document.querySelector(".numero");
+const botonIncrementar = document.querySelector(".btn-incrementar");
+const botonDecrementar = document.querySelector(".btn-decrementar");
+const botonReset = document.querySelector(".reset");
+
+let contador = 0;
+
+const actualizarContador = () => {
+  console.log(contador);
+  numero.textContent = contador;
+};
+
+botonIncrementar.addEventListener("click", () => {
+  contador++;
+  actualizarContador();  
+})
+
+botonDecrementar.addEventListener("click", () => {
+  contador--;
+  actualizarContador();
+})
+
+botonReset.addEventListener("click", () => {
+  contador = 0;
+  actualizarContador();
+})
